2ff283b335d0b26042104675309482330c684e1b
superchargez <87804931+superchargez@users.noreply.github.com>
2023-02-21 16:18:16 +0500

    Update and rename fastSQLOCR.py to fastSQL_OCR.py
    made few changes as well

--- fastSQL_OCR.py
+++ fastSQL_OCR.py
@@ -1,9 +1,8 @@
+#%%
 import cv2, easyocr, os
-ft =[[(296, 750), (1023, 850)],
-[(1570, 750), (2350, 850)],
-[(380, 843), (1020, 920)],
-[(1220, 843), (2343, 930)],
-[(323, 1653), (1053, 1780)]] # fullSized_textLocation
+import matplotlib.pyplot as plt
+import sizes
+ft = sizes.p768
 reader = easyocr.Reader(['en'], detector='DB', recognizer = 'Transformer', gpu=False)
 mydata = []
 import os
@@ -11,7 +10,7 @@ path = os.path.join(os.getcwd()+'/generated_SOF')
 images = os.listdir(path)
 #%%
 import sqlite3
-conn = sqlite3.connect('test.db')
+conn = sqlite3.connect('OCRdatabase.db')
 c = conn.cursor()
 c.execute("""CREATE TABLE IF NOT EXISTS textElements (
 Image_name text,
@@ -21,10 +20,11 @@ Mobile text,
 Email text,
 Order_Number text
 )""")
-checklist = list()
+# checklist = list()
 c.execute("SELECT  * FROM textElements")
-files = c.fetchall()
-for f in files:    checklist.append(f[5])
+# files = c.fetchall()
+# for f in files: checklist.append(f[5])
+checklist = [f[5] for f in c.fetchall()]
 #%%
 for image in images:
     img = cv2.imread(os.path.join(path,image), 1)
@@ -35,6 +35,9 @@ for image in images:
         cropped = img[r[0][1]:r[1][1],r[0][0]:r[1][0]]
         textoutput = reader.readtext(cropped)[0][1]
         mydata.append(textoutput)
+        print(textoutput)
+        # plt.imshow(cropped)
+        # plt.show()
     c.execute("INSERT INTO textElements VALUES (?, ?, ?, ?, ?, ?)", (image, *mydata))
     mydata = []
 conn.commit()
