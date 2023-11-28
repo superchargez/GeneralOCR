a2c6bf0e7c554c0f2d0d2683352add6196e20f14
superchargez <87804931+superchargez@users.noreply.github.com>
2023-11-28 17:15:16 +0500

    updated correctinos

--- corrections.py
+++ corrections.py
@@ -1,6 +1,7 @@
 import re
 from icecream import ic
 def corrected_source(text):
+    ic(text)
     # Remove single or double characters and '@', excluding specific patterns
     text = re.sub(r'\b(?!1\b|Q2\b|1Q2\b|yv1Qi\b|1Q1\b|1Qi\b|v1\b|4G\b)\w{1,2}\b|@', '', text)
     # replace misrepresented text with correct text
@@ -18,6 +19,8 @@ def corrected_source(text):
     elif any(substring in text for substring in ["yv1Qi", "1Q1", "1Qi", "1 Q1", "1 Qi", "Qi"]):
         text = re.sub(r"(Sources)(.*)", r"\1 Upaisa", text)
     source_name = re.search(r'Sources\s*(.*)', text).group(1)
+    source_name = source_name.strip()
+    ic(source_name)
     return source_name
 
 
@@ -29,12 +32,17 @@ def corrected_date_comments(text):
     if match1:
         date = match1.group(1)  # Extracted date
         completed_items_str = match1.group(2)  # Extracted number of completed items
+        completed_items_str = completed_items_str.strip()
+        comment_date = date
         # Check if the completed items string is a number
         if completed_items_str.replace(',', '').isdigit():
-            return date, completed_items_str
+            ic(date, int(completed_items_str))
+            return date, int(completed_items_str)
         else:
             return date, None
     # Return None if no match is found
+    comment_date = date
+    ic(comment_date, completed_items_str)
     return None, None
 
 # def corrected_date_of_comments(text):
@@ -50,19 +58,22 @@ def corrected_date_of_replies(text):
     text = text.replace('Noy', 'Nov')
     match = re.search(r'Activity from (\w+ \d+, \d+)', text)
     date = match.group(1)  # Extracted date    completed_items1 = match1.group(2)  # Extracted number of completed items
-    # dict_r = {'Replies Date': date}
-    # print(dict_r)
-    return date #dict_r
+    reply_date = date
+    ic(reply_date)
+    return date
 
 def corrected_time(text):
-    # ic(text)
+    ic(text)
     # Correct time format
     match = re.search(r'((\d+) hours? )?(\d+) minutes?', text)
     if match is not None:
         hours = int(match.group(2)) if match.group(2) else 0
         minutes = int(match.group(3))
         text = hours * 60 + minutes
-    else: text = ""
+    # else: text = ""
+    else: None
+    time = text
+    ic(time)
     return text
 
 def corrected_replies(text):
@@ -71,6 +82,11 @@ def corrected_replies(text):
     if match:
         number_str = match.group()
         number_str = number_str.replace(',', '')  # Remove commas
+        replies = int(number_str)  # Convert to integer
+        ic(replies)
         return int(number_str)  # Convert to integer
     else:
+        replies = text
+        ic(replies)
         return None
+    
--- main.py
+++ main.py
@@ -14,7 +14,7 @@ def process_image(img_name, region_info, correction_function=None):
     text = pytesseract.image_to_string(region, config=config)
     if correction_function is not None:
         if correction_function.__name__ == 'corrected_source':
-            source_name = corrections.corrected_source(text) # text2source_name
+            source_name = corrections.corrected_source(text)
             logo_name = logo_matcher.logo_match(region)
             platform_name = os.path.splitext(logo_name)[0]
             dict = platform_name +" " + source_name
@@ -35,26 +35,29 @@ def process_image(img_name, region_info, correction_function=None):
     temp_dict['Image Name'] = img_name
     temp_dict['Image Type'] = 'Replies' if img_name.endswith('r.png') else 'Comments'
     # ic(temp_dict)
-    # cv2.imshow('img', region)
-    # cv2.waitKey(0)
+    cv2.imshow('img', region)
+    cv2.waitKey(0)
     return temp_dict
 
 df = pd.DataFrame()
 for img_name in os.listdir(imgdir):
-    result_dict = {}  # Reset the dictionary for each new image
-    if img_name.endswith('r.png'):
-        result_dict.update(process_image(img_name, replies[0], corrections.corrected_date_of_replies))
-        result_dict.update(process_image(img_name, replies[1], corrections.corrected_source))
-        result_dict.update(process_image(img_name, replies[2]))  # No correction function for replies
-        result_dict.update(process_image(img_name, replies[3], corrections.corrected_time))
-    elif img_name.endswith('c.png'):
-        result_dict.update(process_image(img_name, comments[0], corrections.corrected_date_comments))
-        result_dict.update(process_image(img_name, comments[1], corrections.corrected_source))
-    # Convert the dictionary to a DataFrame and append it to the main DataFrame
-    ic(result_dict)
-    temp_df = pd.DataFrame(result_dict, index=[0])
-    df = pd.concat([df, temp_df], ignore_index=True)
+    if img_name.endswith('.png'):
+        result_dict = {}  # Reset the dictionary for each new image
+        if img_name.endswith('r.png'):
+            result_dict.update(process_image(img_name, replies[0], corrections.corrected_date_of_replies))
+            result_dict.update(process_image(img_name, replies[1], corrections.corrected_source))
+            result_dict.update(process_image(img_name, replies[2]))  # No correction function for replies
+            result_dict.update(process_image(img_name, replies[3], corrections.corrected_time))
+        elif img_name.endswith('c.png'):
+            result_dict.update(process_image(img_name, comments[0], corrections.corrected_date_comments))
+            result_dict.update(process_image(img_name, comments[1], corrections.corrected_source))
+        # Convert the dictionary to a DataFrame and append it to the main DataFrame
+        ic(result_dict)
+        temp_df = pd.DataFrame(result_dict, index=[0])
+        df = pd.concat([df, temp_df], ignore_index=True)
 
 # Set 'Date' as the index and save the DataFrame to a CSV file
 df.set_index('Date', inplace=True)
 df.to_csv('input.csv')
+
+# 21r 77=7, 36c 14=4, 45c 111=m1, 50c 111=m1, 52c 13=B, 
